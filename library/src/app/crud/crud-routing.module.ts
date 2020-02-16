c728be43c529e4c6b62b436b88baee73c2f2208d
Oktay Feim Faik <oktayfeimfaik99ies@gmail.com>
2020-02-16 20:42:05 +0100

    Minor fixes

--- library/src/app/crud/crud-routing.module.ts
+++ library/src/app/crud/crud-routing.module.ts
@@ -1,12 +1,7 @@
-import { LibroComponent } from './componentes/libros/libro/libro.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AuthGuard } from '../servicios/guard.service';
 
-
-const routes: Routes = [
-   
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
